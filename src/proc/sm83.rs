d9d4978d11f300e6bb5f79dc831d3be372ebca5a
Matthew D. Steele <mdsteele@alum.mit.edu>
2025-09-20 20:21:56 -0400

    Use dis::sm83::decode_opcode in proc::sm83

--- src/proc/sm83.rs
+++ src/proc/sm83.rs
@@ -1,5 +1,8 @@
 use crate::bus::{SimBus, WatchKind};
-use crate::dis::sm83::{disassemble_instruction, format_instruction};
+use crate::dis::sm83::{
+    Condition, Operation, Reg8, Reg16, decode_opcode, disassemble_instruction,
+    format_instruction,
+};
 use crate::proc::{SimBreak, SimProc};
 
 //===========================================================================//
@@ -13,15 +16,6 @@ const REG_F_MASK: u8 = PROC_FLAG_Z | PROC_FLAG_N | PROC_FLAG_H | PROC_FLAG_C;
 
 //===========================================================================//
 
-/// A condition under which a conditional instruction will be executed.
-enum Cond {
-    Always,
-    C,
-    Nc,
-    Z,
-    Nz,
-}
-
 /// Describes the state of the IME flag.
 enum Ime {
     /// Interrupts are disabled.
@@ -34,26 +28,6 @@ enum Ime {
     Pending1,
     /// Interrupts are enabled.
     Enabled,
-}
-
-/// A general-purpose 8-bit register (for an SM83 processor).
-enum Reg8 {
-    A,
-    B,
-    C,
-    D,
-    E,
-    H,
-    L,
-    Mhl,
-}
-
-/// A general-purpose 16-bit register (for an SM83 processor).
-enum Reg16 {
-    Bc,
-    De,
-    Hl,
-    Sp,
 }
 
 fn pack(hi: u8, lo: u8) -> u16 {
@@ -108,13 +82,19 @@ impl SharpSm83 {
     fn op_call(
         &mut self,
         bus: &mut dyn SimBus,
-        cond: Cond,
+        cond: Condition,
     ) -> Result<(), SimBreak> {
         let dest: u16 = self.read_immediate_word(bus);
         if self.condition_met(cond) {
             self.push_word(bus, self.pc)?;
             self.pc = dest;
         }
+        Ok(())
+    }
+
+    fn op_ccf(&mut self) -> Result<(), SimBreak> {
+        self.reg_f &= !(PROC_FLAG_N | PROC_FLAG_H);
+        self.reg_f ^= PROC_FLAG_C;
         Ok(())
     }
 
@@ -139,7 +119,7 @@ impl SharpSm83 {
     fn op_jp_u16(
         &mut self,
         bus: &mut dyn SimBus,
-        cond: Cond,
+        cond: Condition,
     ) -> Result<(), SimBreak> {
         let dest: u16 = self.read_immediate_word(bus);
         if self.condition_met(cond) {
@@ -156,7 +136,7 @@ impl SharpSm83 {
     fn op_jr(
         &mut self,
         bus: &mut dyn SimBus,
-        cond: Cond,
+        cond: Condition,
     ) -> Result<(), SimBreak> {
         let offset: i8 = self.read_immediate_byte(bus) as i8;
         if self.condition_met(cond) {
@@ -231,12 +211,6 @@ impl SharpSm83 {
         Ok(())
     }
 
-    fn op_pop_af(&mut self, bus: &mut dyn SimBus) -> Result<(), SimBreak> {
-        (self.reg_a, self.reg_f) = unpack(self.pop_word(bus)?);
-        self.reg_f &= REG_F_MASK;
-        Ok(())
-    }
-
     fn op_pop_r16(
         &mut self,
         bus: &mut dyn SimBus,
@@ -245,10 +219,6 @@ impl SharpSm83 {
         let word = self.pop_word(bus)?;
         self.set_r16(reg, word);
         Ok(())
-    }
-
-    fn op_push_af(&mut self, bus: &mut dyn SimBus) -> Result<(), SimBreak> {
-        self.push_word(bus, pack(self.reg_a, self.reg_f))
     }
 
     fn op_push_r16(
@@ -262,7 +232,7 @@ impl SharpSm83 {
     fn op_ret(
         &mut self,
         bus: &mut dyn SimBus,
-        cond: Cond,
+        cond: Condition,
     ) -> Result<(), SimBreak> {
         if self.condition_met(cond) {
             self.pc = self.pop_word(bus)?;
@@ -273,20 +243,26 @@ impl SharpSm83 {
     fn op_rst(
         &mut self,
         bus: &mut dyn SimBus,
-        dest: u16,
+        zp: u8,
     ) -> Result<(), SimBreak> {
         self.push_word(bus, self.pc)?;
-        self.pc = dest;
+        self.pc = u16::from(zp);
         Ok(())
     }
 
-    fn condition_met(&self, cond: Cond) -> bool {
+    fn op_scf(&mut self) -> Result<(), SimBreak> {
+        self.reg_f &= !(PROC_FLAG_N | PROC_FLAG_H);
+        self.reg_f |= PROC_FLAG_C;
+        Ok(())
+    }
+
+    fn condition_met(&self, cond: Condition) -> bool {
         match cond {
-            Cond::Always => true,
-            Cond::C => (self.reg_f & PROC_FLAG_C) != 0,
-            Cond::Nc => (self.reg_f & PROC_FLAG_C) == 0,
-            Cond::Z => (self.reg_f & PROC_FLAG_Z) != 0,
-            Cond::Nz => (self.reg_f & PROC_FLAG_Z) == 0,
+            Condition::Always => true,
+            Condition::C => (self.reg_f & PROC_FLAG_C) != 0,
+            Condition::Nc => (self.reg_f & PROC_FLAG_C) == 0,
+            Condition::Z => (self.reg_f & PROC_FLAG_Z) != 0,
+            Condition::Nz => (self.reg_f & PROC_FLAG_Z) == 0,
         }
     }
 
@@ -334,7 +310,28 @@ impl SharpSm83 {
             Reg8::E => self.reg_e,
             Reg8::H => self.reg_h,
             Reg8::L => self.reg_l,
-            Reg8::Mhl => bus.read_byte(pack(self.reg_h, self.reg_l) as u32),
+            Reg8::Mbc => {
+                let addr = self.get_r16(Reg16::Bc);
+                bus.read_byte(u32::from(addr))
+            }
+            Reg8::Mde => {
+                let addr = self.get_r16(Reg16::De);
+                bus.read_byte(u32::from(addr))
+            }
+            Reg8::Mhl => {
+                let addr = self.get_r16(Reg16::Hl);
+                bus.read_byte(u32::from(addr))
+            }
+            Reg8::Mhli => {
+                let addr = self.get_r16(Reg16::Hl);
+                self.set_r16(Reg16::Hl, addr.wrapping_add(1));
+                bus.read_byte(u32::from(addr))
+            }
+            Reg8::Mhld => {
+                let addr = self.get_r16(Reg16::Hl);
+                self.set_r16(Reg16::Hl, addr.wrapping_sub(1));
+                bus.read_byte(u32::from(addr))
+            }
         }
     }
 
@@ -347,14 +344,34 @@ impl SharpSm83 {
             Reg8::E => self.reg_e = byte,
             Reg8::H => self.reg_h = byte,
             Reg8::L => self.reg_l = byte,
+            Reg8::Mbc => {
+                let addr = self.get_r16(Reg16::Bc);
+                bus.write_byte(u32::from(addr), byte);
+            }
+            Reg8::Mde => {
+                let addr = self.get_r16(Reg16::De);
+                bus.write_byte(u32::from(addr), byte);
+            }
             Reg8::Mhl => {
-                bus.write_byte(pack(self.reg_h, self.reg_l) as u32, byte)
+                let addr = self.get_r16(Reg16::Hl);
+                bus.write_byte(u32::from(addr), byte);
+            }
+            Reg8::Mhli => {
+                let addr = self.get_r16(Reg16::Hl);
+                self.set_r16(Reg16::Hl, addr.wrapping_add(1));
+                bus.write_byte(u32::from(addr), byte);
+            }
+            Reg8::Mhld => {
+                let addr = self.get_r16(Reg16::Hl);
+                self.set_r16(Reg16::Hl, addr.wrapping_sub(1));
+                bus.write_byte(u32::from(addr), byte);
             }
         }
     }
 
     fn get_r16(&self, reg: Reg16) -> u16 {
         match reg {
+            Reg16::Af => pack(self.reg_a, self.reg_f),
             Reg16::Bc => pack(self.reg_b, self.reg_c),
             Reg16::De => pack(self.reg_d, self.reg_e),
             Reg16::Hl => pack(self.reg_h, self.reg_l),
@@ -364,6 +381,10 @@ impl SharpSm83 {
 
     fn set_r16(&mut self, reg: Reg16, word: u16) {
         match reg {
+            Reg16::Af => {
+                (self.reg_a, self.reg_f) = unpack(word);
+                self.reg_f &= REG_F_MASK;
+            }
             Reg16::Bc => (self.reg_b, self.reg_c) = unpack(word),
             Reg16::De => (self.reg_d, self.reg_e) = unpack(word),
             Reg16::Hl => (self.reg_h, self.reg_l) = unpack(word),
@@ -397,7 +418,7 @@ impl SimProc for SharpSm83 {
     }
 
     fn register_names(&self) -> &'static [&'static str] {
-        &["A", "F", "B", "C", "D", "E", "H", "L", "AF", "BC", "DE", "HL"]
+        &["A", "F", "B", "C", "D", "E", "H", "L", "AF", "BC", "DE", "HL", "SP"]
     }
 
     fn get_register(&self, name: &str) -> Option<u32> {
@@ -414,6 +435,7 @@ impl SimProc for SharpSm83 {
             "BC" => Some(u32::from(pack(self.reg_b, self.reg_c))),
             "DE" => Some(u32::from(pack(self.reg_d, self.reg_e))),
             "HL" => Some(u32::from(pack(self.reg_h, self.reg_l))),
+            "SP" => Some(u32::from(self.sp)),
             _ => None,
         }
     }
@@ -435,6 +457,7 @@ impl SimProc for SharpSm83 {
             "BC" => (self.reg_b, self.reg_c) = unpack((value & 0xffff) as u16),
             "DE" => (self.reg_d, self.reg_e) = unpack((value & 0xffff) as u16),
             "HL" => (self.reg_h, self.reg_l) = unpack((value & 0xffff) as u16),
+            "SP" => self.sp = value as u16,
             _ => {}
         };
     }
@@ -442,131 +465,65 @@ impl SimProc for SharpSm83 {
     fn step(&mut self, bus: &mut dyn SimBus) -> Result<(), SimBreak> {
         let opcode = bus.read_byte(self.pc as u32);
         self.pc = self.pc.wrapping_add(1);
-        match opcode {
-            0x00 => self.op_nop(),
-            0x01 => self.op_ld_r16_u16(bus, Reg16::Bc),
-            0x06 => self.op_ld_r8_u8(bus, Reg8::B),
-            0x0e => self.op_ld_r8_u8(bus, Reg8::C),
-            0x11 => self.op_ld_r16_u16(bus, Reg16::De),
-            0x16 => self.op_ld_r8_u8(bus, Reg8::D),
-            0x18 => self.op_jr(bus, Cond::Always),
-            0x1e => self.op_ld_r8_u8(bus, Reg8::E),
-            0x20 => self.op_jr(bus, Cond::Nz),
-            0x21 => self.op_ld_r16_u16(bus, Reg16::Hl),
-            0x26 => self.op_ld_r8_u8(bus, Reg8::H),
-            0x28 => self.op_jr(bus, Cond::Z),
-            0x2e => self.op_ld_r8_u8(bus, Reg8::L),
-            0x30 => self.op_jr(bus, Cond::Nc),
-            0x31 => self.op_ld_r16_u16(bus, Reg16::Sp),
-            0x36 => self.op_ld_r8_u8(bus, Reg8::Mhl),
-            0x38 => self.op_jr(bus, Cond::C),
-            0x3e => self.op_ld_r8_u8(bus, Reg8::A),
-            0x40 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::B),
-            0x41 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::C),
-            0x42 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::D),
-            0x43 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::E),
-            0x44 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::H),
-            0x45 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::L),
-            0x46 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::Mhl),
-            0x47 => self.op_ld_r8_r8(bus, Reg8::B, Reg8::A),
-            0x48 => self.op_ld_r8_r8(bus, Reg8::C, Reg8::B),
-            0x49 => self.op_ld_r8_r8(bus, Reg8::C, Reg8::C),
-            0x4a => self.op_ld_r8_r8(bus, Reg8::C, Reg8::D),
-            0x4b => self.op_ld_r8_r8(bus, Reg8::C, Reg8::E),
-            0x4c => self.op_ld_r8_r8(bus, Reg8::C, Reg8::H),
-            0x4d => self.op_ld_r8_r8(bus, Reg8::C, Reg8::L),
-            0x4e => self.op_ld_r8_r8(bus, Reg8::C, Reg8::Mhl),
-            0x4f => self.op_ld_r8_r8(bus, Reg8::C, Reg8::A),
-            0x50 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::B),
-            0x51 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::C),
-            0x52 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::D),
-            0x53 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::E),
-            0x54 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::H),
-            0x55 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::L),
-            0x56 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::Mhl),
-            0x57 => self.op_ld_r8_r8(bus, Reg8::D, Reg8::A),
-            0x58 => self.op_ld_r8_r8(bus, Reg8::E, Reg8::B),
-            0x59 => self.op_ld_r8_r8(bus, Reg8::E, Reg8::C),
-            0x5a => self.op_ld_r8_r8(bus, Reg8::E, Reg8::D),
-            0x5b => self.op_ld_r8_r8(bus, Reg8::E, Reg8::E),
-            0x5c => self.op_ld_r8_r8(bus, Reg8::E, Reg8::H),
-            0x5d => self.op_ld_r8_r8(bus, Reg8::E, Reg8::L),
-            0x5e => self.op_ld_r8_r8(bus, Reg8::E, Reg8::Mhl),
-            0x5f => self.op_ld_r8_r8(bus, Reg8::E, Reg8::A),
-            0x60 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::B),
-            0x61 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::C),
-            0x62 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::D),
-            0x63 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::E),
-            0x64 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::H),
-            0x65 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::L),
-            0x66 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::Mhl),
-            0x67 => self.op_ld_r8_r8(bus, Reg8::H, Reg8::A),
-            0x68 => self.op_ld_r8_r8(bus, Reg8::L, Reg8::B),
-            0x69 => self.op_ld_r8_r8(bus, Reg8::L, Reg8::C),
-            0x6a => self.op_ld_r8_r8(bus, Reg8::L, Reg8::D),
-            0x6b => self.op_ld_r8_r8(bus, Reg8::L, Reg8::E),
-            0x6c => self.op_ld_r8_r8(bus, Reg8::L, Reg8::H),
-            0x6d => self.op_ld_r8_r8(bus, Reg8::L, Reg8::L),
-            0x6e => self.op_ld_r8_r8(bus, Reg8::L, Reg8::Mhl),
-            0x6f => self.op_ld_r8_r8(bus, Reg8::L, Reg8::A),
-            0x70 => self.op_ld_r8_r8(bus, Reg8::Mhl, Reg8::B),
-            0x71 => self.op_ld_r8_r8(bus, Reg8::Mhl, Reg8::C),
-            0x72 => self.op_ld_r8_r8(bus, Reg8::Mhl, Reg8::D),
-            0x73 => self.op_ld_r8_r8(bus, Reg8::Mhl, Reg8::E),
-            0x74 => self.op_ld_r8_r8(bus, Reg8::Mhl, Reg8::H),
-            0x75 => self.op_ld_r8_r8(bus, Reg8::Mhl, Reg8::L),
-            0x77 => self.op_ld_r8_r8(bus, Reg8::Mhl, Reg8::A),
-            0x78 => self.op_ld_r8_r8(bus, Reg8::A, Reg8::B),
-            0x79 => self.op_ld_r8_r8(bus, Reg8::A, Reg8::C),
-            0x7a => self.op_ld_r8_r8(bus, Reg8::A, Reg8::D),
-            0x7b => self.op_ld_r8_r8(bus, Reg8::A, Reg8::E),
-            0x7c => self.op_ld_r8_r8(bus, Reg8::A, Reg8::H),
-            0x7d => self.op_ld_r8_r8(bus, Reg8::A, Reg8::L),
-            0x7e => self.op_ld_r8_r8(bus, Reg8::A, Reg8::Mhl),
-            0x7f => self.op_ld_r8_r8(bus, Reg8::A, Reg8::A),
-            0xc0 => self.op_ret(bus, Cond::Nz),
-            0xc1 => self.op_pop_r16(bus, Reg16::Bc),
-            0xc2 => self.op_jp_u16(bus, Cond::Nz),
-            0xc3 => self.op_jp_u16(bus, Cond::Always),
-            0xc4 => self.op_call(bus, Cond::Nz),
-            0xc5 => self.op_push_r16(bus, Reg16::Bc),
-            0xc7 => self.op_rst(bus, 0x00),
-            0xc8 => self.op_ret(bus, Cond::Z),
-            0xc9 => self.op_ret(bus, Cond::Always),
-            0xca => self.op_jp_u16(bus, Cond::Z),
-            0xcc => self.op_call(bus, Cond::Z),
-            0xcd => self.op_call(bus, Cond::Always),
-            0xcf => self.op_rst(bus, 0x08),
-            0xd0 => self.op_ret(bus, Cond::Nc),
-            0xd1 => self.op_pop_r16(bus, Reg16::De),
-            0xd2 => self.op_jp_u16(bus, Cond::Nc),
-            0xd3 | 0xdb | 0xdd | 0xe3 | 0xe4 | 0xeb | 0xec | 0xed | 0xf4
-            | 0xfc | 0xfd => self.op_invalid(opcode),
-            0xd4 => self.op_call(bus, Cond::Nc),
-            0xd5 => self.op_push_r16(bus, Reg16::De),
-            0xd7 => self.op_rst(bus, 0x10),
-            0xd8 => self.op_ret(bus, Cond::C),
-            0xda => self.op_jp_u16(bus, Cond::C),
-            0xdc => self.op_call(bus, Cond::C),
-            0xdf => self.op_rst(bus, 0x18),
-            0xe0 => self.op_ldh_u8_a(bus),
-            0xe1 => self.op_pop_r16(bus, Reg16::Hl),
-            0xe2 => self.op_ldh_mc_a(bus),
-            0xe5 => self.op_push_r16(bus, Reg16::Hl),
-            0xe7 => self.op_rst(bus, 0x20),
-            0xe9 => self.op_jp_hl(),
-            0xef => self.op_rst(bus, 0x28),
-            0xf0 => self.op_ldh_a_u8(bus),
-            0xf1 => self.op_pop_af(bus),
-            0xf2 => self.op_ldh_a_mc(bus),
-            0xf3 => self.op_di(),
-            0xf5 => self.op_push_af(bus),
-            0xf7 => self.op_rst(bus, 0x30),
-            0xf9 => self.op_ld_sp_hl(),
-            0xfb => self.op_ei(),
-            0xff => self.op_rst(bus, 0x38),
-            // TODO: implement remaining opcodes
-            _ => Err(SimBreak::HaltOpcode("unimplemented", opcode)),
+        match decode_opcode(opcode) {
+            Operation::AdcAI8 => todo!(),
+            Operation::AdcAR8(_reg) => todo!(),
+            Operation::AddHlR16(_reg) => todo!(),
+            Operation::AddAI8 => todo!(),
+            Operation::AddAR8(_reg) => todo!(),
+            Operation::AddSpI8 => todo!(),
+            Operation::AndAI8 => todo!(),
+            Operation::AndAR8(_reg) => todo!(),
+            Operation::CallM16(cond) => self.op_call(bus, cond),
+            Operation::Ccf => self.op_ccf(),
+            Operation::CpAI8 => todo!(),
+            Operation::CpAR8(_reg) => todo!(),
+            Operation::Cpl => todo!(),
+            Operation::Daa => todo!(),
+            Operation::DecR16(_reg) => todo!(),
+            Operation::DecR8(_reg) => todo!(),
+            Operation::Di => self.op_di(),
+            Operation::Ei => self.op_ei(),
+            Operation::Halt => todo!(),
+            Operation::IncR16(_reg) => todo!(),
+            Operation::IncR8(_reg) => todo!(),
+            Operation::Invalid => self.op_invalid(opcode),
+            Operation::JpI16(cond) => self.op_jp_u16(bus, cond),
+            Operation::JpHl => self.op_jp_hl(),
+            Operation::JrI8(cond) => self.op_jr(bus, cond),
+            Operation::LdAM16 => todo!(),
+            Operation::LdM16A => todo!(),
+            Operation::LdM16Sp => todo!(),
+            Operation::LdHlSpI8 => todo!(),
+            Operation::LdR16I16(reg) => self.op_ld_r16_u16(bus, reg),
+            Operation::LdR8I8(reg) => self.op_ld_r8_u8(bus, reg),
+            Operation::LdR8R8(r1, r2) => self.op_ld_r8_r8(bus, r1, r2),
+            Operation::LdSpHl => self.op_ld_sp_hl(),
+            Operation::LdhAM8 => self.op_ldh_a_u8(bus),
+            Operation::LdhAMc => self.op_ldh_a_mc(bus),
+            Operation::LdhM8A => self.op_ldh_u8_a(bus),
+            Operation::LdhMcA => self.op_ldh_mc_a(bus),
+            Operation::Nop => self.op_nop(),
+            Operation::OrAI8 => todo!(),
+            Operation::OrAR8(_reg) => todo!(),
+            Operation::Pop(reg) => self.op_pop_r16(bus, reg),
+            Operation::Prefix => todo!(),
+            Operation::Push(reg) => self.op_push_r16(bus, reg),
+            Operation::Ret(cond) => self.op_ret(bus, cond),
+            Operation::Reti => todo!(),
+            Operation::Rla => todo!(),
+            Operation::Rlca => todo!(),
+            Operation::Rra => todo!(),
+            Operation::Rrca => todo!(),
+            Operation::Rst(zp) => self.op_rst(bus, zp),
+            Operation::SbcAI8 => todo!(),
+            Operation::SbcAR8(_reg) => todo!(),
+            Operation::Scf => self.op_scf(),
+            Operation::Stop => todo!(),
+            Operation::SubAI8 => todo!(),
+            Operation::SubAR8(_reg) => todo!(),
+            Operation::XorAI8 => todo!(),
+            Operation::XorAR8(_reg) => todo!(),
         }?;
         if let Ime::Pending1 = self.ime {
             self.ime = Ime::Enabled;
